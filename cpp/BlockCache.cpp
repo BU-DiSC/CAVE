8ffe86d8bfb6cd9021818a6a89391969049d85a0
nestflow <20128868+nestflow@users.noreply.github.com>
2023-07-24 16:57:41 -0400

    Cleanup codes and add comments.

--- cpp/BlockCache.cpp
+++ cpp/BlockCache.cpp
@@ -18,52 +18,62 @@ template <class T> std::shared_ptr<T> BlockCache<T>::get_block(int block_id) {
   std::shared_ptr<T> ret_ptr;
 
   index_mtx.lock();
-  bool is_cached = cache_map.find(block_id) != cache_map.end();
+  auto cache_map_iter = cache_map.find(block_id);
 
-  // If cached
-  if (is_cached) {
+  // If cached (Reader)
+  if (cache_map_iter != cache_map.end()) {
     // Get the cached block
-    int cache_idx = cache_map.at(block_id);
+    int cache_idx = cache_map_iter->second;
     assert(c_to_bid[cache_idx] == block_id);
-    {
-      std::shared_lock read_lk(cache_mtx[cache_idx]);
-      index_mtx.unlock();
-      ret_ptr = cache_list[cache_idx];
-      cache_usage_count[cache_idx]++;
-    }
-  } else {
+
+    // Lock cached block, unlock index mutex
+    cache_mtx[cache_idx].lock_shared();
+    index_mtx.unlock();
+
+    ret_ptr = cache_list[cache_idx];
+    cache_usage_count[cache_idx]++;
+
+    cache_mtx[cache_idx].unlock_shared();
+  } else { // If not (Writer)
     int new_cache_idx;
     while (true) {
-      if (cache_usage_count[clock_hand] == 0 &&
-          cache_mtx[clock_hand].try_lock()) {
-        int old_block_id = c_to_bid[clock_hand];
-        if (old_block_id != -1)
-          cache_map.erase(old_block_id);
-        cache_map[block_id] = clock_hand;
-
-        c_to_bid[clock_hand] = block_id;
-        cache_usage_count[clock_hand] = 1;
-        new_cache_idx = clock_hand;
-
+      // Obtain lock of the cache block
+      if (cache_usage_count[clock_hand] > 0) {
+        cache_usage_count[clock_hand]--;
         // Move the clock hand
         clock_hand++;
         if (clock_hand == cache_size)
           clock_hand = 0;
+      } else {
+        // If any threads are using this block?
+        if (cache_mtx[clock_hand].try_lock()) {
+          // Remove old block from cache_map
+          int old_block_id = c_to_bid[clock_hand];
+          if (old_block_id != -1)
+            cache_map.erase(old_block_id);
+          cache_map[block_id] = clock_hand;
 
-        index_mtx.unlock();
+          // Modify flags on cached block
+          c_to_bid[clock_hand] = block_id;
+          cache_usage_count[clock_hand] = 1;
+          new_cache_idx = clock_hand;
 
-        std::shared_ptr<T> eb = sz->read_block<T>(block_id);
-        ret_ptr = eb;
-        cache_list[new_cache_idx] = eb;
-        cache_mtx[new_cache_idx].unlock();
-        break;
+          // Move the clock hand
+          clock_hand++;
+          if (clock_hand == cache_size)
+            clock_hand = 0;
+
+          // Unlock index mtx
+          index_mtx.unlock();
+
+          // Actual load data to cache block
+          std::shared_ptr<T> eb = sz->read_block<T>(block_id);
+          ret_ptr = eb;
+          cache_list[new_cache_idx] = eb;
+          cache_mtx[new_cache_idx].unlock();
+          break;
+        }
       }
-      if (cache_usage_count[clock_hand] > 0)
-        cache_usage_count[clock_hand]--;
-      // Move the clock hand
-      clock_hand++;
-      if (clock_hand == cache_size)
-        clock_hand = 0;
     }
   }
 
